f16b1895d3e7080b21382e23447567cacfd6efa8
Blake <blake.denham@gmail.com>
2019-08-14 17:28:43 +0200

    Prototype v1

---
+++ MirrorGame/Assets/Scripts/MirrorEnter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorEnter : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //var normal = transform.forward;
+        //other.gameObject.GetComponent<PlayerMove>().MirrorEnter(normal);
+        var level = GameObject.FindGameObjectWithTag("Level");
+        transform.parent = null;
+        level.transform.parent = transform;
+        Debug.Log("awe");
+        transform.localScale = new Vector3(
+            -1 * transform.localScale.x, 1, 1);
+        transform.localRotation = Quaternion.LookRotation(-1f * transform.forward);
+        level.transform.parent = null;
+        transform.parent = level.transform;
+    }
+}
---
+++ MirrorGame/Assets/Scripts/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] private GameObject mirror;
+    [SerializeField] private float hitOffset = 0.1f;
+
+    private GameObject level;
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = GameObject.FindGameObjectWithTag("Level");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit,
+                Mathf.Infinity))
+            {
+                var m = Instantiate(mirror, level.transform);
+                m.transform.position = hit.point + hit.normal * hitOffset;
+                m.transform.rotation = Quaternion.LookRotation(hit.normal);
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance,
+                    Color.yellow);
+            }
+        }
+    }
+}
