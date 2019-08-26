797d7c38b331251ce208682b8f5a687bfd9aefd4
Blake <blake.denham@gmail.com>
2019-08-26 13:18:02 +0200

    Reflectables

--- MirrorGame/Assets/Scripts/MirrorEnter.cs
+++ MirrorGame/Assets/Scripts/MirrorEnter.cs
@@ -9,11 +9,17 @@ public class MirrorEnter : MonoBehaviour
     {
         
         var level = GameObject.FindGameObjectWithTag("Level");
-        transform.parent = null;
-        level.transform.parent = transform;
-        transform.localScale = new Vector3(
-            -1 * transform.localScale.x, 1, 1);
-        transform.localRotation = Quaternion.LookRotation(-1f * transform.forward);
+        foreach (var reflectable in level.GetComponentsInChildren<Reflectable>())
+        {
+            reflectable.Reflect();
+        }
+        
+        var mirrorTransform = transform;
+        mirrorTransform.parent = null;
+        level.transform.parent = mirrorTransform;
+        mirrorTransform.localScale = new Vector3(
+            -1 * mirrorTransform.localScale.x, 1, 1);
+        transform.localRotation = Quaternion.LookRotation(-1f * mirrorTransform.forward);
         level.transform.parent = null;
         transform.parent = level.transform;
     }
---
+++ MirrorGame/Assets/Scripts/Reflectable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using UnityEngine;
+
+public class Reflectable : MonoBehaviour
+{
+
+    [SerializeField] private String ColourReference;
+    [SerializeField] private Color reflectedColour;
+    private Color _default;
+    private Material _material;
+    private bool _isReflected;
+
+    private void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+        _default = _material.GetColor(ColourReference);
+    }
+
+    public void Reflect()
+    {
+        _isReflected = !_isReflected;
+        _material.SetColor(ColourReference, _isReflected?reflectedColour:_default);
+    }
+}
