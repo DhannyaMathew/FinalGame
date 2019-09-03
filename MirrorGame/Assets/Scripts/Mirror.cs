c5fd023eb924455947759a03ff6251eb62655b86
Blake <blake.denham@gmail.com>
2019-09-03 13:46:03 +0200

    Level1 changes

--- MirrorGame/Assets/Scripts/Mirror.cs
+++ MirrorGame/Assets/Scripts/Mirror.cs
@@ -2,29 +2,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.HDPipeline;
+
+[Serializable]
+public class LinearMapping
+{
+    public Vector2 minMaxIn;
+    public Vector2 minMaxOut;
+    public bool clamp = true;
+
+    public float Evaluate(float x)
+    {
+        var raw = (minMaxOut.x - minMaxOut.y) / (minMaxIn.x - minMaxIn.y) * (x - minMaxIn.x) + minMaxOut.x;
+        return clamp ? Mathf.Clamp(raw, minMaxOut.x, minMaxOut.y) : raw;
+    }
+}
 
 public class Mirror : MonoBehaviour
 {
-    [SerializeField] private Vector2 minMaxDistance = new Vector2(3f, 7f);
-    [SerializeField] private Vector2 minMaxDistortionStrength = new Vector2(0.001f, 0.1f);
+    [SerializeField] private LinearMapping distortion;
+    [SerializeField] private LinearMapping fov;
+
+
     private Material _material;
+    private HDProbe _prp;
     private static readonly int NormalStrength = Shader.PropertyToID("_normalStrength");
 
     private void Awake()
     {
         _material = GetComponentInChildren<Renderer>().material;
+        _prp = GetComponentInChildren<HDProbe>();
+        Debug.Log(_prp);
+
     }
 
     private void Update()
     {
         //y=(y1-y0)/(x1-x0)(x-x0)+y0
         var playerPosition = GameManager.PlayerTransform.position;
-        var strength = (minMaxDistortionStrength.x - minMaxDistortionStrength.y) /
-                       (minMaxDistance.x - minMaxDistance.y) *
-                       ((playerPosition - transform.position).magnitude - minMaxDistance.x) +
-                       minMaxDistortionStrength.x;
-        strength = Mathf.Clamp(strength, minMaxDistortionStrength.x, minMaxDistortionStrength.y);
+        var dist = (playerPosition - transform.position).magnitude;
+        var strength = distortion.Evaluate(dist);
+        var fov = this.fov.Evaluate(dist);
         _material.SetFloat(NormalStrength, strength);
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -40,7 +60,7 @@ public class Mirror : MonoBehaviour
         level.transform.parent = mirrorTransform;
         mirrorTransform.localScale = new Vector3(
             -1 * mirrorTransform.localScale.x, 1, 1);
-        transform.localRotation = Quaternion.LookRotation(-1f * mirrorTransform.forward);
+        //transform.localRotation = Quaternion.LookRotation(-1f * mirrorTransform.forward);
         level.transform.parent = null;
         transform.parent = level.transform;
     }
