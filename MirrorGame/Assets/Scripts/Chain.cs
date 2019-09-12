65602a874868f12c6d5db551ac74304f5f36cf72
Blake <blake.denham@gmail.com>
2019-09-13 01:49:33 +0200

    Stuff

---
+++ MirrorGame/Assets/Scripts/Chain.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Chain : MonoBehaviour
+{
+    private Link[] _links;
+
+    private Link Root => _links.Length > 0 ? _links[0] : null;
+    private int Length => _links.Length;
+
+    private void Awake()
+    {
+        _links = GetComponentsInChildren<Link>();
+    }
+
+    private void Update()
+    {
+        for (int i = Length - 1; i >= 1; i--)
+        {
+            _links[i].transform.position = _links[i-1].Anchor.position;
+        }
+    }
+}
---
+++ MirrorGame/Assets/Scripts/Link.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public Transform Anchor { get; private set; }
+    
+    
+    private void Start()
+    {
+        Anchor = transform.GetChild(0).GetChild(0);
+    }
+}
