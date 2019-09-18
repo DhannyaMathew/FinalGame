f96f8a5e2fd3600e3b787e9f323b0f928169bbd6
MarcoDP <marcoprinsloo072@gmail.com>
2019-09-18 21:18:38 +0200

    Worked on Soundtrack Script

--- MirrorGame/Assets/Scripts/Soundtrack_Script.cs
+++ MirrorGame/Assets/Scripts/Soundtrack_Script.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Soundtrack_Script : MonoBehaviour
 {
+	public AudioSource Soundtrack_Source ;
+	public AudioClip Clip01 ;
     // Start is called before the first frame update
     void Start()
     {
-        
+		Soundtrack_Source = this.GetComponent <AudioSource> () ;
+		Soundtrack_Source.clip = Clip01 ;
     }
 
     // Update is called once per frame
